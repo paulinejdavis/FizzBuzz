77e9b96f7f3409c376f28405f2232926c25758e0
tourmal <tomoliverallen@gmail.com>
2022-09-05 11:53:59 +0000

    my working solution/spec pair

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,3 @@
 def fizzbuzz(num)
-    'fizz'
+   return((num % 15).zero? ? "fizzbuzz" : (num % 3).zero? ? "fizz" : (num % 5).zero? ? "buzz" : num)
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,16 @@
-require ./lib/'fizzbuzz'
+require './lib/fizzbuzz'
 
 describe 'fizzbuzz' do
-    it  'returns "fizz" when passed 3' do
+    it  'returns "fizzbuzz" when passed a multiple of 3 and 5' do
+        expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    end
+    it  'returns "fizz" when passed a multiple of 3' do
         expect(fizzbuzz(3)).to eq 'fizz'
+    end
+    it  'returns "buzz" when passed a multiple of 5' do
+        expect(fizzbuzz(5)).to eq 'buzz'
+    end
+    it  'returns echoes when passed none of the above' do
+        expect(fizzbuzz(4)).to eq 4
     end
 end
