5c7b78fe5312e6d9151329a2b72f8d12fc136279
ppjd <pauljdavi.t21@btinternet.com>
2022-09-05 11:39:30 +0100

    fizzbuzz - passed 3

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,14 @@
+describe 'fizzbuzz' do
+    it  'returns "fizz" when passed 3' do
+        expect(fizzbuzz(3)).to eq 'fizz'
+    end
+end
+
+
+
+
+# describe 'fizzbuzz' do
+#     it 'returns "fizz" when passed 3' do
+#       expect(fizzbuzz(3)).to eq 'fizz'
+#     end
+#   end
