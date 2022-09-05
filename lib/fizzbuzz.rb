7ffde64886f6cbfc46e033d9bae554ec71e493fc
Tom Allen <111132028+tomallens@users.noreply.github.com>
2022-09-05 15:12:05 +0100

    refactor of fizzbuzz.rb

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,3 @@
 def fizzbuzz(num)
-    if num % 3 == 0 && num % 5 != 0
-        return "fizz"
-    elsif num % 5 == 0 && num % 3 != 0
-        return "buzz"
-    elsif num % 3 == 0 && num % 5 == 0
-        return "fizzbuzz"
-    else
-        return num
-    end    
+return((num % 15).zero? ? "fizzbuzz" : (num % 3).zero? ? "fizz" : (num % 5).zero? ? "buzz" : num)
 end
