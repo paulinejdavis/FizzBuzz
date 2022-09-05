bf62f5980bd55743a791568d7f62abcc6b3a0f4c
ppjd <pauljdavi.t21@btinternet.com>
2022-09-05 12:13:47 +0100

    test & code

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,11 @@
 def fizzbuzz(num)
-    'fizz'
+    if num % 3 == 0 && num % 5 != 0
+        return "fizz"
+    elsif num % 5 == 0 && num % 3 != 0
+        return "buzz"
+    elsif num % 3 == 0 && num % 5 == 0
+        return "fizzbuzz"
+    else
+        return num
+    end    
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,20 @@
-require ./lib/'fizzbuzz'
+# require ./lib/'fizzbuzz'
+require 'fizzbuzz'
 
 describe 'fizzbuzz' do
     it  'returns "fizz" when passed 3' do
-        expect(fizzbuzz(3)).to eq 'fizz'
-    end
+       expect(fizzbuzz(3)).to eq('fizz')
+        end
+        
+    it 'returns buzz if number is divisible by 5' do
+        expect(fizzbuzz(5)).to eq('buzz')
+        end
+        
+    it 'returns fizzbuzz if number is divisible by 3 and 5' do
+        expect(fizzbuzz(15)).to eq('fizzbuzz')
+        end
+        
+    it 'returns number if number is not divisible by 3 or 5' do
+        expect(fizzbuzz(1)).to eq(1)
+        end
 end
